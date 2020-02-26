2f53f8bfdad7df0bfadc439ee556e8d740205830
Michael Gale <m.gale@warwick.ac.uk>
2020-02-26 13:43:32 +0000

    Add code from lecture

--- src/JSON.hs
+++ src/JSON.hs
@@ -31,6 +31,45 @@ data Value = Obj Object
 -- instance Applicative Parser
 -- instance Monad Parser
 
+quote :: Parser Char 
+quote = ch (=='"')
 
+keyword :: String -> Parser () 
+keyword [] = pure ()
+keyword (x:xs) = do 
+    ch (==x)
+    keyword xs
+    pure ()
+
+sepBy :: Parser a -> Parser sep -> Parser [a]
+sepBy p sep = go <|> pure []
+    where go = do 
+            r <- p
+            rs <- many (sep *> p)
+            pure (r:rs)
+
+arrayP :: Parser Value 
+arrayP = Arr <$> between (ch (=='[')) (ch (==']')) (sepBy valueP (ch (==',')))
+
+nullP :: Parser Value 
+nullP = do 
+    keyword "null"
+    pure Null
+
+trueP :: Parser Value 
+trueP = do 
+    keyword "true"
+    pure (Bool True)   
+
+falseP :: Parser Value 
+falseP = do 
+    keyword "false"
+    pure (Bool False)
+
+numP :: Parser Value 
+numP = Num <$> nat
+
+valueP :: Parser Value 
+valueP = token (nullP <|> trueP <|> falseP <|> numP <|> arrayP)
 
 --------------------------------------------------------------------------------
